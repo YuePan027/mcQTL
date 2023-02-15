f24f024a56782060c111e6ad90a0ab6c9005dedb
Yue <yuep027@gmail.com>
2023-02-15 12:54:44 -0600

    update deconv function

---
+++ R/deconvolution.R
@@ -0,0 +1,45 @@
+#' Estimation of cellular composition in high-throughput data from heterogeneous tissues
+#'
+#' This function returns a `SummarizedExperiment` object including cell-type proportion estimates for each sample.
+#'
+#' This is a function developed to implement cell-type proportion deconvolution using either `CIBERSORT` or `nnls`.
+#'
+#' @param se A `SummarizedExperiment` object.
+#' @param method A character string denotes which deconvolution method to use. In thi current version, only `CIBERSORT` or `nnls` is supported.
+#'
+#' @return A `SummarizedExperiment` with the cell-type proportion estimates for each sample stored in metadata slot.
+#'
+#' @examples
+#'
+#' se <- SummarizedExperiment(assays = list(counts = pQTL::protein_data), rowData = pQTL::anno_protein)
+#' se <- deconv(se, "cibersort")
+#'
+#'
+deconv <- function(se,
+                   method = c("cibersort", "nnls")){
+  in_use <- intersect(rownames(assay(se)), rownames(se@metadata$sig_matrix))
+  protein_sub <- assay(se)[in_use, , drop=F]
+  sig_matrix_sub <- se@metadata$sig_matrix[in_use, , drop=F]
+
+  if(method == "cibersort"){
+    result <- CIBERSORT(sig_matrix = sig_matrix_sub,
+                        mixture_file = protein_sub,
+                        perm=0, QN=TRUE,
+                        absolute=FALSE, abs_method='sig.score')
+    prop <- result[, 1:ncol(sig_matrix_sub)]
+  }
+
+  if(method == "nnls"){
+    decon_nnls <- apply(protein_sub, 2, function(y)nnls(sig_matrix_sub,y)$x) %>%t
+    prop <- decon_nnls/rowSums(decon_nnls)
+    rownames(prop) <- colnames(protein_sub)
+    colnames(prop) <- colnames(sig_matrix_sub)
+  }
+
+  res_tca <- tca(X = protein_sub,
+                 W = prop, refit_W = T,
+                 verbose = FALSE)
+  prop <- res_tca$W
+  se@metadata$prop <- prop
+  return(se)
+}
